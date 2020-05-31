6fba602980b25b93fba667be8fd5c255eb2f909b
Brusenenko <brusenenko@yandex.ru>
2020-05-31 17:36:48 +0300

    Simple app interface

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/client.fxml"));
+        primaryStage.setTitle("GB Cloud Storage");
+        primaryStage.setScene(new Scene(root, 900, 400));
+        primaryStage.show();
     }
 }
